d538b0f2f2aee8d965663e605d575d29b847e6f5
Vsevolod Popov <sevapopov13@gmail.com>
2023-09-20 12:03:47 +0300

    API test. Add new user creation test.

--- test_reqresin_api.py
+++ test_reqresin_api.py
@@ -2,8 +2,20 @@
 
 import requests
 
+
 def test_get_list_of_users():
     '''Get list of users with an API.'''
     URL = 'https://reqres.in/api/users'
     response = requests.get(URL)
+    assert response.status_code == 200
+
+
+def test_create_new_user():
+    '''Create new user with API.'''
+    URL = 'https://reqers.in/api/users'
+    data = {
+        'name': 'Vsevolod Popov',
+        'movies': ['Live until Monday', 'Where is nophelet located?']
+    }
+    response = requests.post(URL, data=data)
     assert response.status_code == 200
